18e7c97008a05b2a1c984beaf79337a4a0c3a37b
tiji1010 <158030422+tiji1010@users.noreply.github.com>
2025-11-17 13:21:03 +0530

    Second Commit By Tijil

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = (roles = []) => (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).json({ message: "❌ No token" });
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("DECODED TOKEN:", decoded);
+
+    if (roles.length && !roles.includes(decoded.role))
+      return res.status(403).json({ message: "❌ Access denied: insufficient role" });
+
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "❌ Invalid token" });
+  }
+};
