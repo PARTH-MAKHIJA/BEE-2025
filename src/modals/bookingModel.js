93efeffc1544365f16ec7d682f49d0ffbd1353af
Shubham Bhardwaj <159251372+bhadwajshubham@users.noreply.github.com>
2025-11-17 12:42:24 +0530

    Delete src/modals/bookingModel.js

--- src/modals/bookingModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const bookingSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  roomId: { type: mongoose.Schema.Types.ObjectId, ref: "Room", required: true },
-}, { timestamps: true });
-
-export default mongoose.model("Booking", bookingSchema);
